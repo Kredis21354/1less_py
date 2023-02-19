a419a168d1e483fe34910b384e7b5fca0950e6f0
Kredis21354 <125788986+Kredis21354@users.noreply.github.com>
2023-02-19 08:09:27 +0000

    added the variable

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 class Person:
-  height = 180
-  name = "Dmytro"
+  name = "Dmytro Prokopets"
+  is_male = True
   age = 14
-  
+  height = 180
+  weight = 60
+
+me = Person()
+you = Person()
+
+
+print(me.height)
+me.height += 5
+print(me.height)
   
